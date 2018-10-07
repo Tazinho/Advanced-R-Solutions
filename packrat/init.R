ff0a08ab493c4e8c7cbb73d1851be15600e10c9f
henning <henningsway@posteo.org>
2018-10-07 14:01:09 +0200

    updated packrat.lock and src/
    - packrat::packify()
    - packrat::restore()
    - handinstalled failing packages
    - repeat until restore() completed
    - updated packrat
    - snapshot()
    
    
    Former-commit-id: 5d6fb00302172e3b8515d4ef80b18b69cd314d81

--- packrat/init.R
+++ packrat/init.R
@@ -68,8 +68,6 @@ local({
   ## or when explicitly asked for on the command line
   if (interactive() || "--bootstrap-packrat" %in% commandArgs(TRUE)) {
 
-    needsRestore <- "--bootstrap-packrat" %in% commandArgs(TRUE)
-
     message("Packrat is not installed in the local library -- ",
             "attempting to bootstrap an installation...")
 
@@ -119,8 +117,7 @@ local({
       }
 
       # Restore the project, unload the temporary packrat, and load the private packrat
-      if (needsRestore)
-        packrat::restore(prompt = FALSE, restart = TRUE)
+      packrat::restore(prompt = FALSE, restart = TRUE)
 
       ## This code path only reached if we didn't restart earlier
       unloadNamespace("packrat")
@@ -143,6 +140,7 @@ local({
     if (!file.exists(lib)) {
       dir.create(lib, recursive = TRUE)
     }
+    lib <- normalizePath(lib, winslash = "/")
 
     message("> Installing packrat into project private library:")
     message("- ", shQuote(lib))
@@ -152,8 +150,7 @@ local({
       paste0(with, x, with)
     }
 
-
-    ## Invoke install.packages() in clean R session
+    ## The following is performed because a regular install.packages call can fail
     peq <- function(x, y) paste(x, y, sep = " = ")
     installArgs <- c(
       peq("pkgs", surround(packratSrcPath, with = "'")),
@@ -161,22 +158,17 @@ local({
       peq("repos", "NULL"),
       peq("type", surround("source", with = "'"))
     )
-
-    fmt <- "utils::install.packages(%s)"
-    installCmd <- sprintf(fmt, paste(installArgs, collapse = ", "))
-
-    ## Write script to file (avoid issues with command line quoting
-    ## on R 3.4.3)
-    installFile <- tempfile("packrat-bootstrap", fileext = ".R")
-    writeLines(installCmd, con = installFile)
-    on.exit(unlink(installFile), add = TRUE)
+    installCmd <- paste(sep = "",
+                        "utils::install.packages(",
+                        paste(installArgs, collapse = ", "),
+                        ")")
 
     fullCmd <- paste(
       surround(file.path(R.home("bin"), "R"), with = "\""),
       "--vanilla",
       "--slave",
-      "-f",
-      surround(installFile, with = "\"")
+      "-e",
+      surround(installCmd, with = "\"")
     )
     system(fullCmd)
 
@@ -186,10 +178,10 @@ local({
     ## an 'installed from source' version
 
     ## -- InstallAgent -- ##
-    installAgent <- "InstallAgent: packrat 0.4.9-7"
+    installAgent <- 'InstallAgent: packrat 0.4.8-1'
 
     ## -- InstallSource -- ##
-    installSource <- "InstallSource: source"
+    installSource <- 'InstallSource: source'
 
     packratDescPath <- file.path(lib, "packrat", "DESCRIPTION")
     DESCRIPTION <- readLines(packratDescPath)
@@ -201,8 +193,7 @@ local({
     library("packrat", character.only = TRUE, lib.loc = lib)
 
     message("> Restoring library")
-    if (needsRestore)
-      packrat::restore(prompt = FALSE, restart = FALSE)
+    restore(restart = FALSE)
 
     # If the environment allows us to restart, do so with a call to restore
     restart <- getOption("restart")
