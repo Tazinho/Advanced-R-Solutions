1cb754f4abe374013f750939e037499a7167736e
Henning <henningsway@posteo.org>
2019-03-14 15:16:09 +0100

    ports Hadleys "common.R" to our project
    - #115

--- common.R
+++ common.R
@@ -1,8 +1,69 @@
+# Specific for this project -------------------------------------
+Sys.setenv(LANGUAGE = "en")
+
+
+# Inherited from Advanced R -------------------------------------
+library(methods)
+set.seed(1014)
+
+# options(lifecycle_warnings_as_errors = TRUE)
+
 knitr::opts_chunk$set(
   comment = "#>",
   collapse = TRUE,
-  cache = TRUE
+  cache = TRUE,
+  fig.retina = 0.8, # figures are either vectors or 300 dpi diagrams
+  dpi = 300,
+  out.width = "70%",
+  fig.align = 'center',
+  fig.width = 6,
+  fig.asp = 0.618,  # 1 / phi
+  fig.show = "hold"
 )
 
-Sys.setenv(LANGUAGE = "en")
-library("methods")
+options(
+  rlang_trace_top_env = rlang::current_env(),
+  rlang__backtrace_on_error = "none"
+)
+
+options(
+  digits = 3,
+  str = strOptions(strict.width = "cut")
+)
+
+if (knitr::is_latex_output()) {
+  knitr::opts_chunk$set(width = 65)  # CHANGED (because of indenting)
+  options(width = 65)                # CHANGED
+  options(crayon.enabled = FALSE)
+  options(cli.unicode = TRUE)
+}
+
+knitr::knit_hooks$set(
+  small_mar = function(before, options, envir) {
+    if (before) {
+      par(mar = c(4.1, 4.1, 0.5, 0.5))
+    }
+  }
+)
+
+# Make error messages closer to base R
+registerS3method("wrap", "error", envir = asNamespace("knitr"),
+                 function(x, options) {
+                   msg <- conditionMessage(x)
+                   
+                   call <- conditionCall(x)
+                   if (is.null(call)) {
+                     msg <- paste0("Error: ", msg)
+                   } else {
+                     msg <- paste0("Error in ", deparse(call)[[1]], ": ", msg)
+                   }
+                   
+                   msg <- error_wrap(msg)
+                   knitr:::msg_wrap(msg, "error", options)
+                 }
+)
+
+error_wrap <- function(x, width = getOption("width")) {
+  lines <- strsplit(x, "\n", fixed = TRUE)[[1]]
+  paste(strwrap(lines, width = width), collapse = "\n")
+}
